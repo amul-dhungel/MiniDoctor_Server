7870e8fec83ea93e073d380aef1106e3ed58e881
amul-dhungel <applier1234@gmail.com>
2020-11-20 10:39:34 +0545

    "Demo auth"

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv/config')
 const express = require('express')
 const mongoose = require('mongoose')
 const port = process.env.port || 5000
 const app = express()
-
 mongoose.connect('mongodb://localhost:27017/AppDB', { useNewUrlParser: true });
+
 
 const connection = mongoose.connection
 connection.once("open", () => {
@@ -14,6 +15,33 @@ connection.once("open", () => {
 app.use(express.json())
 const userRoute = require('./routes/user')
 app.use('/user', userRoute)
+
+// phone authentication
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const client = require('twilio')(accountSid, authToken)
+
+app.get('/login', (req, res) => {
+    client.messages.create({
+        body: "This is the text",
+        from: '+12604758096',
+        to: '+9779819067945'
+    })
+        .then((message) => console.log(message))
+        .catch((err) => console.log(err))
+
+    // .verify
+    // .verifications
+    // .create({
+    //     to: `+${req.query.phonenumber}`,
+    //     channel: req.query.channel
+    // })
+    // .then((data) => {
+    //     res.status(200).send(data)
+    // })
+})
+
 
 app.route("/").get((req, res) => res.json("hello world"))
 
