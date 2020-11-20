72a9bd78fbef7054eae9e5e7e129ac0d5c4be1dd
amul-dhungel <applier1234@gmail.com>
2020-11-20 08:50:04 +0545

    "user commit"

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,31 @@ const User = require('../models/users_model')
 
 const router = express.Router()
 
+router.route('/:username').get((req, res) => {
+    User.find().exec().
+        then(result => {
+            res.status(200).json({
+                details: result
+            })
+        }).catch(err => console.log(err))
+})
+
+router.route('/login').post((req, res) => {
+    User.findOne({ username: req.body.username }, (err, result) => {
+        if (err) return res.status(500).json({ msg: err })
+        if (result === null) {
+            return res.status(403).json("Username incorrect")
+        }
+        if (result.password === req.body.password) {
+
+            // here we implement the JWT token functionality
+            res.json("ok")
+        } else {
+            res.status(403).json("Password is incorrect")
+        }
+    })
+})
+
 router.route('/register').post((req, res) => {
     console.log("Inside the register");
     const user = new User({
@@ -38,7 +63,14 @@ router.route('/update/:username').patch((req, res) => {
 })
 
 router.route('/delete/:username').delete((req, res) => {
-
+    User.findOneAndDelete({ username: req.params.username }, (err, result) => {
+        if (err) return status(500).json({ msg: err })
+        const msg = {
+            msg: "username deleted",
+            username: req.params.username,
+        }
+        return res.json(msg)
+    })
 })
 
 
