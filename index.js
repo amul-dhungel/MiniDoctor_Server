59a28b57638c8ae86178b8b55602a688c35e900e
amul-dhungel <applier1234@gmail.com>
2020-11-21 09:30:49 +0545

    "heroku setup"

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require('express')
 const mongoose = require('mongoose')
 const port = process.env.PORT || 5000
 const app = express()
-// mongoose.connect('mongodb+srv://admin:admin@minidoctor.3ef6h.mongodb.net/AppDB?retryWrites=true&w=majority', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://admin:admin@minidoctor.3ef6h.mongodb.net/AppDB?retryWrites=true&w=majority', { useNewUrlParser: true });
 
 
-// const connection = mongoose.connection
-// connection.once("open", () => {
-//     console.log("Mongodb Connected")
-// })
+const connection = mongoose.connection
+connection.once("open", () => {
+    console.log("Mongodb Connected")
+})
 
-//middleware .
+middleware.
 
-app.use(express.json())
+    app.use(express.json())
 const userRoute = require('./routes/user')
 app.use('/user', userRoute)
 
