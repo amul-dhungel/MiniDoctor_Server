640200b851e9827144b173aad8afc672f1fd9a42
amul-dhungel <applier1234@gmail.com>
2020-11-21 09:27:30 +0545

    port fixed

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 const app = express()
-mongoose.connect('mongodb+srv://admin:admin@minidoctor.3ef6h.mongodb.net/AppDB?retryWrites=true&w=majority', { useNewUrlParser: true });
+// mongoose.connect('mongodb+srv://admin:admin@minidoctor.3ef6h.mongodb.net/AppDB?retryWrites=true&w=majority', { useNewUrlParser: true });
 
 
-const connection = mongoose.connection
-connection.once("open", () => {
-    console.log("Mongodb Connected")
-})
+// const connection = mongoose.connection
+// connection.once("open", () => {
+//     console.log("Mongodb Connected")
+// })
 
 //middleware .
 
